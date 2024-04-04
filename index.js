9628b023e10bcdd54b5fc6c49dff4aef6742f634
Margaret <mungaimargaretn@gmail.com>
2024-04-04 21:30:41 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainTag = document.querySelector("#main")
+
+mainTag.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Margaret is the champion"
